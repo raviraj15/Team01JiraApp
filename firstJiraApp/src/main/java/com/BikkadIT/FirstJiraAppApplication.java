01566f047baaed1be88df0c65cf23c92d2bbbd54
raviraj15 <ravirajmandlik.rm@gmail.com>
2022-11-11 14:32:29 +0530

    TEAM-1:create project in spring boot

---
+++ firstJiraApp/src/main/java/com/BikkadIT/FirstJiraAppApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJiraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJiraAppApplication.class, args);
+	}
+
+}
